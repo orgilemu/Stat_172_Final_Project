3186adbcc68c2e348aa9b76a6e54e4b340ef0d0c
Kendall Starcevich <kendall.starcevich@drake.edu>
2025-12-06 18:08:14 -0600

    added interpretations

--- src/interpretation_and_prediction.R
+++ src/interpretation_and_prediction.R
@@ -85,6 +85,14 @@ summary(final_descriptive_glm)
 #there is definitely some complete separation here, there are some huge standard errors (Religion type all over 100)
 #so, we need to use firths (logistf)
 
+#We also want to force "Unapplicable" to be the reference level for all Type columns, to make it much easier to interpret
+# The relevel() function moves a specific level to the front (baseline) - AI USAGE HERE
+
+model_data$Race.Type <- relevel(model_data$Race.Type, ref = "Unapplicable")
+model_data$Sex.Type  <- relevel(model_data$Sex.Type, ref = "Unapplicable")
+model_data$National.Origin.Type <- relevel(model_data$National.Origin.Type, ref = "Unapplicable")
+model_data$Religion.Type <- relevel(model_data$Religion.Type, ref = "Unapplicable")
+
 final_descriptive_glm <- logistf(Outcome_bin ~ Processor + Housing + 
                                Public.Accommodations + Education + Credit +
                                Race.Type + Disability + Age + Sex.Type +
@@ -93,87 +101,52 @@ final_descriptive_glm <- logistf(Outcome_bin ~ Processor + Housing +
                              data = model_data, 
                              family = binomial(link = "logit"))
 
-summary(final_descriptive_glm)
-
+# summary(final_descriptive_glm)
+# 
 # Model fitted by Penalized ML
 # Coefficients:
-#   coef   se(coef)  lower 0.95  upper 0.95        Chisq            p
-# (Intercept)                                                    -4.44821326 1.56461068 -9.39710000 -2.00780916 15.983204500 6.390693e-05
-# ProcessorICRC                                                   1.19921054 0.12851352  0.95526522  1.46059684          Inf 0.000000e+00
-# ProcessorLocal                                                  1.40113351 0.13821256  1.13673099  1.68019603          Inf 0.000000e+00
-# HousingYes                                                      1.35149891 0.05038112  1.25259326  1.45034512          Inf 0.000000e+00
-# Public.AccommodationsYes                                       -0.77778814 0.07929394 -0.93643685 -0.62440232          Inf 0.000000e+00
-# EducationYes                                                   -0.34434353 0.16135033 -0.67528837 -0.03899378  4.933454238 2.634161e-02
-# CreditYes                                                      -0.66086998 0.49564854 -1.79615374  0.21591005  2.062398798 1.509723e-01
-# Race.TypeAsian or Pacific Islander                              0.55672614 0.40271452 -0.20454587  1.39589458  2.016157503 1.556325e-01
-# Race.TypeBlack                                                  0.29380315 0.34618538 -0.33502984  1.04124132  0.770822052 3.799629e-01
-# Race.TypeOther                                                  0.54966360 0.35669519 -0.10355468  1.31421143  2.660775029 1.028506e-01
-# Race.TypeWhite                                                  0.18393115 0.37106930 -0.50298053  0.97155029  0.252459772 6.153484e-01
-# Race.TypeUnapplicable                                           0.83498743 0.34538528  0.20796882  1.58113762  7.177316956 7.383104e-03
-# DisabilityYes                                                   0.18106387 0.04467979  0.09321376  0.26876530 16.282310819 5.457102e-05
-# AgeYes                                                         -0.27537858 0.05817246 -0.39062088 -0.16207702 23.219135956 1.445518e-06
-# Sex.TypeMale                                                   -0.43238478 0.09498818 -0.62161316 -0.24801032 21.808295621 3.012946e-06
-# Sex.TypeUnapplicable                                           -0.23703160 0.05170897 -0.33818554 -0.13507719 20.529760974 5.871127e-06
-# PregnancyYes                                                    0.38083501 0.10065938  0.18035659  0.57556953 13.476900544 2.415184e-04
-# National.Origin.TypeEast Indian                                -1.56001611 1.48992739 -6.45418309  0.66132887  1.624691054 2.024389e-01
-# National.Origin.TypeHispanic                                   -0.14536896 0.40700954 -0.91121307  0.70637981  0.123725958 7.250280e-01
-# National.Origin.TypeMexican                                    -0.14006996 0.41515201 -0.92424557  0.72530584  0.110690994 7.393588e-01
-# National.Origin.TypeOther National Origin                       0.25614012 0.38534694 -0.46168230  1.07149650  0.455254736 4.998501e-01
-# National.Origin.TypePerceived as Afghani, Arab, Middle Eastern  0.38787610 0.96257256 -1.95061835  2.08456843  0.150542264 6.980177e-01
-# National.Origin.TypeUnapplicable                                0.30953540 0.38176174 -0.40010983  1.11904793  0.684788406 4.079427e-01
-# Marital.StatusYes                                              -2.07765796 0.82737537 -4.27045136 -0.73801066 11.642722300 6.445421e-04
-# Religion.TypeCatholic                                           0.09586721 1.55230801 -2.35533225  5.03210653  0.003868968 9.504027e-01
-# Religion.TypeJewish                                            -0.68448071 1.69314855 -3.75494454  4.35182844  0.146037408 7.023513e-01
-# Religion.TypeMuslim                                             0.76191267 1.48818374 -1.46009559  5.65136791  0.318842611 5.723040e-01
-# Religion.TypeOther                                             -0.43114794 1.48069043 -2.62842376  4.45274802  0.074990060 7.842052e-01
-# Religion.TypeProtestant                                        -1.12687856 1.57002335 -3.65202764  3.82201612  0.401063675 5.265404e-01
-# Religion.TypeSikhs                                              2.20507814 2.20217477 -3.21099870  7.65632176  0.906100983 3.411511e-01
-# Religion.TypeUnapplicable                                       0.52109384 1.47208941 -1.64785340  5.39865497  0.144039438 7.042978e-01
-# CreedYes                                                        0.32264063 0.39660839 -0.54208787  1.04489265  0.606108061 4.362570e-01
-# ColorYes                                                        0.06075982 0.07449792 -0.08623115  0.20686552  0.658947026 4.169319e-01
-# Sexual.OrientationYes                                           0.07392136 0.09338351 -0.11274472  0.25457141  0.614886441 4.329539e-01
-# method
-# (Intercept)                                                         2
-# ProcessorICRC                                                       2
-# ProcessorLocal                                                      2
-# HousingYes                                                          2
-# Public.AccommodationsYes                                            2
-# EducationYes                                                        2
-# CreditYes                                                           2
-# Race.TypeAsian or Pacific Islander                                  2
-# Race.TypeBlack                                                      2
-# Race.TypeOther                                                      2
-# Race.TypeWhite                                                      2
-# Race.TypeUnapplicable                                               2
-# DisabilityYes                                                       2
-# AgeYes                                                              2
-# Sex.TypeMale                                                        2
-# Sex.TypeUnapplicable                                                2
-# PregnancyYes                                                        2
-# National.Origin.TypeEast Indian                                     2
-# National.Origin.TypeHispanic                                        2
-# National.Origin.TypeMexican                                         2
-# National.Origin.TypeOther National Origin                           2
-# National.Origin.TypePerceived as Afghani, Arab, Middle Eastern      2
-# National.Origin.TypeUnapplicable                                    2
-# Marital.StatusYes                                                   2
-# Religion.TypeCatholic                                               2
-# Religion.TypeJewish                                                 2
-# Religion.TypeMuslim                                                 2
-# Religion.TypeOther                                                  2
-# Religion.TypeProtestant                                             2
-# Religion.TypeSikhs                                                  2
-# Religion.TypeUnapplicable                                           2
-# CreedYes                                                            2
-# ColorYes                                                            2
-# Sexual.OrientationYes                                               2
+#                                                                       coef   se(coef)  lower 0.95  upper 0.95       Chisq            p method
+# (Intercept)                                                    -3.01962819 0.13075879 -3.28519796 -2.77099332         Inf 0.000000e+00      2
+# ProcessorICRC                                                   1.19921054 0.12851352  0.95526522  1.46059684         Inf 0.000000e+00      2
+# ProcessorLocal                                                  1.40113351 0.13821256  1.13673099  1.68019603         Inf 0.000000e+00      2
+# HousingYes                                                      1.35149891 0.05038112  1.25259326  1.45034512         Inf 0.000000e+00      2
+# Public.AccommodationsYes                                       -0.77778814 0.07929394 -0.93643685 -0.62440232         Inf 0.000000e+00      2
+# EducationYes                                                   -0.34434353 0.16135033 -0.67528837 -0.03899378  4.93345424 2.634161e-02      2
+# CreditYes                                                      -0.66086998 0.49564854 -1.79615374  0.21591005  2.06239880 1.509723e-01      2
+# Race.TypeAmerican Indian                                       -0.83498743 0.34538528 -1.58113762 -0.20796882  7.17731696 7.383104e-03      2
+# Race.TypeAsian or Pacific Islander                             -0.27826129 0.21624476 -0.72351544  0.13142056  1.72913676 1.885212e-01      2
+# Race.TypeBlack                                                 -0.54118428 0.06362959 -0.66687314 -0.41690274         Inf 0.000000e+00      2
+# Race.TypeOther                                                 -0.28532384 0.10988287 -0.50489390 -0.07210619  6.94358207 8.412049e-03      2
+# Race.TypeWhite                                                 -0.65105628 0.14557520 -0.94721414 -0.37366548 22.99201682 1.626755e-06      2
+# DisabilityYes                                                   0.18106387 0.04467979  0.09321376  0.26876530 16.28231082 5.457102e-05      2
+# AgeYes                                                         -0.27537858 0.05817246 -0.39062088 -0.16207702 23.21913596 1.445518e-06      2
+# Sex.TypeFemale                                                  0.23703160 0.05170897  0.13507719  0.33818554 20.52976097 5.871127e-06      2
+# Sex.TypeMale                                                   -0.19535318 0.08892463 -0.37319187 -0.02337321  4.97833512 2.566667e-02      2
+# PregnancyYes                                                    0.38083501 0.10065938  0.18035659  0.57556953 13.47690054 2.415184e-04      2
+# National.Origin.TypeAfghani, Arab, Middle Eastern              -0.30953540 0.38176174 -1.11904793  0.40010983  0.68478841 4.079427e-01      2
+# National.Origin.TypeEast Indian                                -1.86955151 1.44220160 -6.72900865  0.17687872  3.02617864 8.193077e-02      2
+# National.Origin.TypeHispanic                                   -0.45490436 0.15116888 -0.76138612 -0.16631813  9.85814295 1.690817e-03      2
+# National.Origin.TypeMexican                                    -0.44960536 0.17347065 -0.80283690 -0.11946785  7.28704938 6.945345e-03      2
+# National.Origin.TypeOther National Origin                      -0.05339528 0.09029548 -0.23354900  0.12192643  0.34986526 5.541894e-01      2
+# National.Origin.TypePerceived as Afghani, Arab, Middle Eastern  0.07834070 0.88838390 -2.17227106  1.57529322  0.00755832 9.307203e-01      2
+# Marital.StatusYes                                              -2.07765796 0.82737537 -4.27045136 -0.73801066 11.64272230 6.445421e-04      2
+# Religion.Type7th Day Adventist                                 -0.52109384 1.47208941 -5.39865497  1.64785340  0.14403944 7.042978e-01      2
+# Religion.TypeCatholic                                          -0.42522663 0.49338407 -1.54984894  0.43730171  0.82846548 3.627166e-01      2
+# Religion.TypeJewish                                            -1.20557455 0.83763737 -3.39757925  0.13159425  2.99439851 8.355293e-02      2
+# Religion.TypeMuslim                                             0.24081883 0.22499203 -0.22186687  0.66773489  1.08450699 2.976915e-01      2
+# Religion.TypeOther                                             -0.95224178 0.16404731 -1.28977023 -0.64244882 43.11135363 5.171130e-11      2
+# Religion.TypeProtestant                                        -1.64797239 0.54653782 -2.93448948 -0.71774748 15.21180537 9.610077e-05      2
+# Religion.TypeSikhs                                              1.68398430 1.63838938 -3.30345272  4.62580568  0.77081653 3.799646e-01      2
+# CreedYes                                                        0.32264063 0.39660839 -0.54208787  1.04489265  0.60610806 4.362570e-01      2
+# ColorYes                                                        0.06075982 0.07449792 -0.08623115  0.20686552  0.65894703 4.169319e-01      2
+# Sexual.OrientationYes                                           0.07392136 0.09338351 -0.11274472  0.25457141  0.61488644 4.329539e-01      2
 # 
 # Method: 1-Wald, 2-Profile penalized log-likelihood, 3-None
 # 
 # Likelihood ratio test=1705.027 on 33 df, p=0, n=23580
 # Wald test = 8469.497 on 33 df, p = 0
 
-
+#------------------------------INTERPRETATIONS-------------------------------------------
 #where you file your case matters - 
 #all else held constant, filing a claim locally has odds of achieving a positive outcome over 4 times greater than those filed 
 #at the EEOC (federal level). We are 95% confident that the true change in odds is between a factor of 3.1 and 5.4.
@@ -181,3 +154,8 @@ summary(final_descriptive_glm)
 #exp(1.40113351)
 #exp(1.13673099)
 #exp(1.68019603)
+
+
+#claims filed 
+
+
